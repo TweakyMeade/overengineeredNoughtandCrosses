bc589f6cfd17a1a9f313907d705a0de61ec1ecbf
Anthony Meade <antnymeade@gmail.com>
2021-02-19 22:32:39 +0000

    Cleanup

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ def XOChoice():
 
         if chooser == "O":
             return int(2)
-        print("You've not made a vaild selection")
+        print("You've not made a valid selection")
 def printBoard(fR, sR, tR):
     print('')
-    fRS= fR[0] + fR[1] + fR[2]
+    fRS = fR[0] + fR[1] + fR[2]
     sRS = sR[0] + sR[1] + sR[2]
     tRS = tR[0] + tR[1] + tR[2]
     print('  123')
-    print('1',fRS)
-    print('2',sRS)
-    print('3',tRS)
+    print('1', fRS)
+    print('2', sRS)
+    print('3', tRS)
 def returnCounter(count):
     if count % 2 == 0:
         return '0'
@@ -38,22 +38,21 @@ def winQuestion(fR, sR, tR):
     if tR[0] == tR[1] == tR[2] != '-':
         return True
     return False
-def playerAction(row,column,pCount):
+def playerAction(row, column, pCount):
     row[column] = returnCounter(pCount)
-
-def inputFunc(pCount,rowOne, rowTwo, rowThree):
+def inputFunc(pCount, rowOne, rowTwo, rowThree):
     x = int(input('Which row do you choose? "1","2" or "3":'))
     y = int(input('Which column do you choose? "1","2" or "3"')) - 1
     if int(x) == 1:
-        if prodAns(rowOne, int(y),):
+        if prodAns(rowOne, int(y), ):
             playerAction(rowOne, int(y), pCount)
             return True
     if int(x) == 2:
-        if prodAns(rowTwo, int(y),):
+        if prodAns(rowTwo, int(y), ):
             playerAction(rowTwo, int(y), pCount)
             return True
     if int(x) == 3:
-        if prodAns(rowThree, int(y),):
+        if prodAns(rowThree, int(y), ):
             playerAction(rowThree, int(y), pCount)
             return True
     return False
@@ -62,33 +61,29 @@ def whoIsPlaying(pCount):
         print('Now its 0!')
     else:
         print('now its X go!')
-def prodAns(row,column):
+def prodAns(row, column):
     return row[column] == '-'
-
-
-
 print("Welcome to 0's and X's!")
-rOne = ['-','-','-']
-rTwo = ['-','-','-']
-rThree = ['-','-','-']
+rOne = ['-', '-', '-']
+rTwo = ['-', '-', '-']
+rThree = ['-', '-', '-']
 playerCount = XOChoice()
 roundCount = 9
 print(playerCount)
 while True:
-    printBoard(rOne,rTwo,rThree)
+    printBoard(rOne, rTwo, rThree)
     whoIsPlaying(playerCount)
-    restarter = inputFunc(playerCount, rOne, rTwo, rThree)
-    if restarter:
+    gRestart = inputFunc(playerCount, rOne, rTwo, rThree)
+    if gRestart:
         playerCount += 1
         roundCount -= 1
     else:
         print('that was an invalid input try again')
     if winQuestion(rOne, rTwo, rThree):
-        print("YOU ARE WINNER:", returnCounter(playerCount-1))
+        print("YOU ARE WINNER:", returnCounter(playerCount - 1))
         printBoard(rOne, rTwo, rThree)
         break
     if roundCount == 0:
         print("NO ONE WINS THE EASIEST GAME IN THE WORLD!")
         break
-
 print("an over thought out solution, to an easy program")
